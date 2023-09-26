64bb54ed01f82d4d2c3a70f993f35beeae95c65f
Arjun Viswanathan <arjun.viswanathan2612@gmail.com>
2023-09-25 22:18:02 -0400

    comments

--- video_object_detection.py
+++ video_object_detection.py
@@ -1,13 +1,15 @@
 '''
 Author: Arjun Viswanathan
+SDP Team 12 ArUco Detection Script
 Date created: 9/25/23
 Date last modified: 9/25/23
-Description: detecting ArUco markers off a live camera feed
+Description: base code for detecting ArUco markers off a live camera feed
 '''
 
 import cv2
 import scipy.io as sio
 
+# Load camera parameters from MATLAB
 camParams = sio.loadmat("camParams.mat")
 cameraMatrix = camParams['cameraMatrix']
 distCoeffs = camParams['distortionCoefficients']
@@ -15,6 +17,7 @@ distCoeffs = camParams['distortionCoefficients']
 camera = cv2.VideoCapture(0)
 success = 1
 
+# Set up the ArUco dictionary and detector object
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_1000)
 aruco_params = cv2.aruco.DetectorParameters()
 detector = cv2.aruco.ArucoDetector(aruco_dict, aruco_params)
@@ -22,6 +25,7 @@ markerLength = 142 # mm
 
 print("Reading from camera...\n")
 
+# To keep track of saved images
 i = 0
 j = 0
 
@@ -29,12 +33,14 @@ while success:
     success, image = camera.read()
     s = image.shape
 
+    # First we detect all markers in the frame
     (corners, ids, rejected) = detector.detectMarkers(image)
-
-    if len(corners) > 0:
+ 
+    if len(corners) > 0: # we have detected something
         ids = ids.flatten()
-        cv2.aruco.drawDetectedMarkers(image, corners, ids)
+        cv2.aruco.drawDetectedMarkers(image, corners, ids) # draw outlines for all 
 
+        # For every detected marker, we do pose estimation using its corners and find the rotational and translational vectors
         for (markerCorner, markerID) in zip(corners, ids):
             reshapedCorners = markerCorner.reshape((4, 2))
             (tL, tR, bR, bL) = reshapedCorners
@@ -44,20 +50,23 @@ while success:
             rvec = rvec[0][0]
             tvec = tvec[0][0]
 
+            # Printing distance on the image
             cv2.putText(image, str(round(tvec[2], 2)), (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
             print("Marker detected! ID: {}, RVEC: {}, TVEC: {}".format(str(markerID), rvec, tvec))
 
+        # Press 's' key when detecting marker to save image. Only available when marker is detected
+        if cv2.waitKey(33) == ord('s'):
+            print("Taking ArUco pic {}...".format(j))
+            cv2.imwrite("Images/aruco_image_{}.png".format(j), image)
+            j += 1
+
     cv2.imshow("ArUCO Detection", image)
 
+    # Press 'a' key to save image for calibration
     if cv2.waitKey(33) == ord('a'):
         print("Taking pic {}...".format(i))
         cv2.imwrite("Images/image_{}.png".format(i), image)
         i += 1
-
-    if cv2.waitKey(33) == ord('s'):
-        print("Taking ArUco pic {}...".format(j))
-        cv2.imwrite("Images/aruco_image_{}.png".format(j), image)
-        j += 1
 
     if cv2.waitKey(1) == 27: # ESC key to exit
         break
