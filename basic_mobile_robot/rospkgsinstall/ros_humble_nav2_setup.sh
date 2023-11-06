1e5e53b81b6a71b717d91bed74303a3901c6f34c
Arjun Viswanathan <41523488+arjuns-code-center@users.noreply.github.com>
2023-11-06 15:29:32 -0500

    added setup files for installations

---
+++ basic_mobile_robot/rospkgsinstall/ros_humble_nav2_setup.sh
@@ -0,0 +1,18 @@
+sudo apt install ros-humble-navigation2
+sudo apt install ros-humble-nav2-bringup
+
+sudo apt install ros-humble-turtlebot3*
+sudo apt install ros-humble-nav2-simple-commander
+
+sudo apt install ros-humble-slam-toolbox
+
+sudo apt install ros-humble-joint-state-publisher-gui
+sudo apt install ros-humble-xacro
+
+sudo apt-get install gedit
+
+sudo apt install ros-humble-gazebo-ros-pkgs
+
+sudo apt-get install ros-humble-rqt-robot-steering
+
+sudo apt install ros-humble-robot-localization
