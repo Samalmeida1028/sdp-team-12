9b10fc9bc3759c3cf25f3a730ec6638834170747
Arjun Viswanathan <arjun.viswanathan2612@gmail.com>
2023-11-07 22:08:01 -0500

    updated files

--- README.md
+++ README.md
@@ -1,18 +1,33 @@
 # sdp-team-12
 This is the repository for Team 12's Senior Design Project (SDP)
 
-```basic_mobile_bot_v1.launch.py``` : uses the ```basic_mobile_bot_v1.urdf``` file to put a robot in RViz to see
-```basic_mobile_bot_v2.launch.py``` : uses the ```basic_mobile_bot_v1.urdf``` file to put a robot in RViz and launch a simulation in Gazebo
-```basic_mobile_bot_v3.launch.py``` : performs sensor fusion for odometry and IMU and publishes to a new topic in a smalltown.world simulation
-```basic_mobile_bot_v4.launch.py``` : shows a smalltown.world simulation for the robot with LiDAR on it using ```basic_mobile_bot_v2.urdf```
+## Navigation
 
-```basic_mobile_bot_v5.launch.py``` : uses Nav2 to localize the robot using LiDAR with and without SLAM in a smalltown world using the Costmap API
+In this branch, there are 2 main packages:
+- basic_mobile_robot
+- navigator
+
+Before running, make sure to run both ```ros_humble_setup.sh``` and ```ros_humble_nav2_setup.sh``` scripts in ```basic_mobile_robot/rospkginstall``` directory, if you do not have ROS2 Humble and/or Nav2 installed
+
+## basic_mobile_robot Package
+
+There are a bunch of launch files that all do different things. Below is an overview. 
+
+```urdf_visualizer.launch.py``` : uses the ```basic_mobile_bot_v1.urdf``` file to put a robot in RViz to see \
+
+```gazebo_simulator.launch.py``` : uses the ```basic_mobile_bot_v1.urdf``` file to put a robot in RViz and launch a simulation in Gazebo \
+
+```sensor_fusion_odom.launch.py``` : performs sensor fusion for odometry and IMU and publishes to a new topic in a smalltown.world simulation \
+
+```lidar_config.launch.py``` : shows a smalltown.world simulation for the robot with LiDAR on it using ```basic_mobile_bot_v2.urdf``` \
+
+```nav_env.launch.py``` : uses Nav2 to localize the robot using LiDAR with and without SLAM in a smalltown world using the Costmap API \
 
 Use ```rqt_robot_steering``` and change the topic to ```cmd_vel``` to use a GUI for moving the robot in Gazebo. 
 
-Things I did so far: 
-1. Set up a URDF and view it in RViz
-2. Set up a SDF for Gazebo to simulate the URDF with rqt_robot_steering
-3. Set up a smalltown.world to simulate the URDF in a world instead of bare Gazebo
-4. Set up a LiDAR on the URDF and simulated that in the smalltown.world
-5. Set it up with Nav2 to do navigation from point A to B with a map
+## navigator Package
+
+This package has code to publish to ```/cmd_vel``` topic through keyboard input as well as publish a random pose to check if navigation works
+
+Points to navigate to are in the the form of ```PoseStamped``` messages. They are published to the ```/goal_pose``` topic. Once this is done, ```/cmd_vel_nav``` will output many ```Twist``` messages for the robot to get to the goal state. 
+
--- navigator/navigator/nav2pose.py
+++ navigator/navigator/nav2pose.py
@@ -1,198 +1,115 @@
 #!/usr/bin/env python3
-# # Author: Arjun Viswanathan
-# # Date created: 11/5/23
-# # Date last modified: 11/5/23
-# # Description: Using Nav2 to navigate to a given pose
-
-# from geometry_msgs.msg import PoseStamped
-# from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
-# import rclpy
-# from rclpy.node import Node
-# from rclpy.duration import Duration
-
-# class Nav2Pose(Node):
-#     def __init__(self):
-#         super().__init__('nav2pose')
-#         self.navigator = BasicNavigator()
-#         # Wait for navigation to fully activate, since autostarting nav2
-#         # self.navigator.lifecycleStartup()
-#         self.navigator.waitUntilNav2Active()
-#         print("Ready!")
-
-#         # self.navigator.changeMap('/mnt/e/UMass_Amherst/SDP/sdp-team-12/basic_mobile_robot/maps/smalltown_world.yaml')
-
-#         self.initial_pose = PoseStamped()
-#         self.goal = PoseStamped()
-
-#         #self.init_navigator(3.45, 2.15, 1.0, 0.0)
-
-#         self.i = 0
-
-#         self.poseSub = self.create_subscription(PoseStamped, '/goal_pose', self.setgoal, 10)
-        
-#         time_period = 0.5
-#         self.timer = self.create_timer(time_period, self.nav2pose_callback)
-
-#     def init_navigator(self, x, y, z, w):
-#         # Set our demo's initial pose
-#         self.initial_pose.header.frame_id = 'map'
-#         self.initial_pose.header.stamp = self.navigator.get_clock().now().to_msg()
-#         self.initial_pose.pose.position.x = x
-#         self.initial_pose.pose.position.y = y
-#         self.initial_pose.pose.orientation.z = z
-#         self.initial_pose.pose.orientation.w = w
-
-#         self.navigator.setInitialPose(self.initial_pose)
-
-#         # If desired, you can change or load the map as well
-#         # self.navigator.changeMap('/path/to/map.yaml')
-
-#         # You may use the navigator to clear or obtain costmaps
-#         # navigator.clearAllCostmaps()  # also have clearLocalCostmap() and clearGlobalCostmap()
-#         # global_costmap = navigator.getGlobalCostmap()
-#         # local_costmap = navigator.getLocalCostmap()
-
-#     def example_nav2pose(self):
-#         goal_pose = PoseStamped()
-#         goal_pose.header.frame_id = 'map'
-#         goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
-#         goal_pose.pose.position.x = -2.0
-#         goal_pose.pose.position.y = -0.5
-#         goal_pose.pose.orientation.w = 1.0
-
-#         self.navigator.goToPose(goal_pose)
-
-#     def setgoal(self, goalmsg):
-#         print("Updated goal pose!")
-#         self.goal = goalmsg
-
-#     def nav2pose_callback(self):
-#         # other way to do this
-#         # path = self.navigator.getPath(self.initial_pose, self.goal)
-#         # smooth_path = self.navigator.smoothPath(path)
-#         # self.navigator.followPath(smooth_path)
-
-#         self.navigator.goToPose(self.goal)
-
-#         if not self.navigator.isTaskComplete():
-#             ################################################
-#             #
-#             # Implement some code here for your application!
-#             #
-#             ################################################
-
-#             # Do something with the feedback
-#             self.i = self.i + 1
-#             feedback = self.navigator.getFeedback()
-#             if feedback and self.i % 5 == 0:
-#                 print('Estimated time of arrival: ' + '{0:.0f}'.format(
-#                     Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
-#                     + ' seconds.')
-
-#                 # Some navigation timeout to cancellation
-#                 if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
-#                     self.navigator.cancelTask()
-
-#         result = self.navigator.getResult()
-#         if result == TaskResult.SUCCEEDED:
-#             print('Goal succeeded!')
-#         elif result == TaskResult.CANCELED:
-#             print('Goal was canceled!')
-#         elif result == TaskResult.FAILED:
-#             print('Goal failed!')
-
-# def main(args=None):
-#     rclpy.init(args=args)
-
-#     nav2pose = Nav2Pose()
-
-#     rclpy.spin(nav2pose)
-
-#     nav2pose.destroy_node()
-#     rclpy.shutdown()
-
-# if __name__ == '__main__':
-#     main()
+# Author: Arjun Viswanathan
+# Date created: 11/5/23
+# Date last modified: 11/7/23
+# Description: Using Nav2 to navigate to a given pose
 
 from geometry_msgs.msg import PoseStamped
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 import rclpy
+from rclpy.node import Node
 from rclpy.duration import Duration
 
-"""
-Basic navigation demo to go to pose.
-"""
+class Nav2Pose(Node):
+    def __init__(self):
+        super().__init__('nav2pose')
+        self.navigator = BasicNavigator()
+        # Wait for navigation to fully activate, since autostarting nav2
+        # self.navigator.lifecycleStartup()
+        self.navigator.waitUntilNav2Active(localizer="bt_navigator")
+        print("Ready!")
 
+        # self.navigator.changeMap('/mnt/e/UMass_Amherst/SDP/sdp-team-12/basic_mobile_robot/maps/smalltown_world.yaml')
 
-def main():
-    rclpy.init()
+        self.initial_pose = PoseStamped()
+        self.goal = PoseStamped()
 
-    navigator = BasicNavigator()
+        #self.init_navigator(3.45, 2.15, 1.0, 0.0)
 
-    # Wait for navigation to fully activate, since autostarting nav2
-    navigator.waitUntilNav2Active(localizer="bt_navigator")
+        self.i = 0
 
-    # If desired, you can change or load the map as well
-    # navigator.changeMap('/path/to/map.yaml')
+        self.poseSub = self.create_subscription(PoseStamped, '/goal_pose', self.setgoal, 10)
+        
+        time_period = 0.5
+        self.timer = self.create_timer(time_period, self.nav2pose_callback)
 
-    # You may use the navigator to clear or obtain costmaps
-    # navigator.clearAllCostmaps()  # also have clearLocalCostmap() and clearGlobalCostmap()
-    # global_costmap = navigator.getGlobalCostmap()
-    # local_costmap = navigator.getLocalCostmap()
+    def init_navigator(self, x, y, z, w):
+        # Set our demo's initial pose
+        self.initial_pose.header.frame_id = 'map'
+        self.initial_pose.header.stamp = self.navigator.get_clock().now().to_msg()
+        self.initial_pose.pose.position.x = x
+        self.initial_pose.pose.position.y = y
+        self.initial_pose.pose.orientation.z = z
+        self.initial_pose.pose.orientation.w = w
 
-    # Go to our demos first goal pose
-    goal_pose = PoseStamped()
-    goal_pose.header.frame_id = 'map'
-    goal_pose.header.stamp = navigator.get_clock().now().to_msg()
-    goal_pose.pose.position.x = -2.0
-    goal_pose.pose.position.y = -0.5
-    goal_pose.pose.orientation.w = 1.0
+        self.navigator.setInitialPose(self.initial_pose)
 
-    # sanity check a valid path exists
-    # path = navigator.getPath(initial_pose, goal_pose)
+        # If desired, you can change or load the map as well
+        # self.navigator.changeMap('/path/to/map.yaml')
 
-    navigator.goToPose(goal_pose)
+        # You may use the navigator to clear or obtain costmaps
+        # navigator.clearAllCostmaps()  # also have clearLocalCostmap() and clearGlobalCostmap()
+        # global_costmap = navigator.getGlobalCostmap()
+        # local_costmap = navigator.getLocalCostmap()
 
-    i = 0
-    while not navigator.isTaskComplete():
-        ################################################
-        #
-        # Implement some code here for your application!
-        #
-        ################################################
+    def example_nav2pose(self):
+        goal_pose = PoseStamped()
+        goal_pose.header.frame_id = 'map'
+        goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
+        goal_pose.pose.position.x = -2.0
+        goal_pose.pose.position.y = -0.5
+        goal_pose.pose.orientation.w = 1.0
 
-        # Do something with the feedback
-        i = i + 1
-        feedback = navigator.getFeedback()
-        if feedback and i % 5 == 0:
-            print('Estimated time of arrival: ' + '{0:.0f}'.format(
-                  Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
-                  + ' seconds.')
+        self.navigator.goToPose(goal_pose)
 
-            # Some navigation timeout to demo cancellation
-            if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
-                navigator.cancelTask()
+    def setgoal(self, goalmsg):
+        print("Updated goal pose!")
+        self.goal = goalmsg
 
-            # Some navigation request change to demo preemption
-            if Duration.from_msg(feedback.navigation_time) > Duration(seconds=18.0):
-                goal_pose.pose.position.x = -3.0
-                navigator.goToPose(goal_pose)
+    def nav2pose_callback(self):
+        # other way to do this
+        # path = self.navigator.getPath(self.initial_pose, self.goal)
+        # smooth_path = self.navigator.smoothPath(path)
+        # self.navigator.followPath(smooth_path)
 
-    # Do something depending on the return code
-    result = navigator.getResult()
-    if result == TaskResult.SUCCEEDED:
-        print('Goal succeeded!')
-    elif result == TaskResult.CANCELED:
-        print('Goal was canceled!')
-    elif result == TaskResult.FAILED:
-        print('Goal failed!')
-    else:
-        print('Goal has an invalid return status!')
+        self.navigator.goToPose(self.goal)
 
-    navigator.lifecycleShutdown()
+        if not self.navigator.isTaskComplete():
+            ################################################
+            #
+            # Implement some code here for your application!
+            #
+            ################################################
 
-    exit(0)
+            # Do something with the feedback
+            self.i = self.i + 1
+            feedback = self.navigator.getFeedback()
+            if feedback and self.i % 5 == 0:
+                print('Estimated time of arrival: ' + '{0:.0f}'.format(
+                    Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
+                    + ' seconds.')
 
+                # Some navigation timeout to cancellation
+                if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
+                    self.navigator.cancelTask()
+
+        result = self.navigator.getResult()
+        if result == TaskResult.SUCCEEDED:
+            print('Goal succeeded!')
+        elif result == TaskResult.CANCELED:
+            print('Goal was canceled!')
+        elif result == TaskResult.FAILED:
+            print('Goal failed!')
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    nav2pose = Nav2Pose()
+
+    rclpy.spin(nav2pose)
+
+    nav2pose.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
