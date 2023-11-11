ba6c31ee083b3349b72a5cac6128a5b433212d9e
CameronReyno <creyno1128@gmail.com>
2023-11-10 19:04:48 -0500

    can send forward and backward command with keys

--- motor_pc_test.py
+++ motor_pc_test.py
@@ -1,6 +1,8 @@
 
 import serial
 import json
+import pynput
+from pynput.keyboard import Key, Listener
 
 ser = serial.Serial(
              'COM4',
@@ -10,8 +12,37 @@ ser = serial.Serial(
     # Used to convert between ROS and OpenCV images
 
 
-while(True):
-  data = "Hello Pico"
+movement_vector = [0.0,0.0]
+
+# while(True):
+#   data = "Hello Pico"
    
-  ser.write(bytearray(data +"\n",encoding="utf-8"))
+#   ser.write(bytearray(data +"\n",encoding="utf-8"))
+def on_press(key):
+    #print('{0} pressed'.format(
+        #key))
+    check_key(key)
+
+def on_release(key):
+    #print('{0} release'.format(
+       # key))
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+def check_key(key):
+    if key is Key.up:
+      movement_vector[0]+=.01
+    if key is Key.down:
+      movement_vector[0]-=.01
+    movement_vector[0]=min(max(-.9,movement_vector[0]),.9)
+    ser.write(bytearray(json.dumps(movement_vector) +"\n",encoding="utf-8"))
+         
+      
+
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
   
