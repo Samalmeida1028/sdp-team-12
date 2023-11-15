a04af611a5667e34c21af78f8e0e81ae97a110c8
Sam Almeida <80793329+Samalmeida1028@users.noreply.github.com>
2023-11-15 14:15:42 -0500

    Update motor_controller.py

--- motor_controller.py
+++ motor_controller.py
@@ -3,6 +3,30 @@ import digitalio
 import pwmio
 
 
+
+
+class Wheel:
+    def __init__(self, PWM, INA, INB,freq) -> None:
+        self.PWM = pwmio.PWMOut(PWM,duty_cycle=0,frequency=freq)
+        self.INA = digitalio.DigitalInOut(INA)
+        self.INB = digitalio.DigitalInOut(INB)
+        self.INA.switch_to_output()
+        self.INB.switch_to_output()
+        self.INA.value = False
+        self.INB.value = False
+        self.duty_cycle = 0
+
+    def set_forward(self):
+        self.INA.value = False
+        self.INB.value = True
+    def set_backward(self):
+        self.INA.value = True
+        self.INB.value = False
+    def stop(self):
+        self.INA.value = False
+        self.INB.value = False
+
+
 class DFR0601:
 
 
@@ -47,6 +71,9 @@ class DFR0601:
         self.PWM2.duty_cycle =int(abs(duty_cycle))
         self.speed_vector = int(duty_cycle)
         
+
+
+
     def forward(self):
         self.INB1.value = True
         self.INB2.value = True
@@ -77,6 +104,30 @@ class DFR0601:
         self.INA1.value = self.INA2.value = self.INB1.value = self.INB2.value = False
         self.PWM1.duty_cycle = self.PWM2.duty_cycle = 0
 
+    
+
+    
+    def wheel_1_forward(self):
+        self.INA1.value = False
+        self.INB1.value = True
+    def wheel_1_backward(self):
+        self.INA1.value = True
+        self.INB1.value = False
+    def wheel_2_forward(self):
+        self.INA2.value = False
+        self.INB2.value = True
+    def wheel_2_backward(self):
+        self.INA2.value = True
+        self.INB2.value = False
+    def wheel_1_stop(self):
+        self.INA1.value = False
+        self.INB1.value = False
+    def wheel_2_stop(self):
+        self.INA2.value = False
+        self.INB2.value = False
+
+    
+
 
 class ChassisController:
 
@@ -85,27 +136,69 @@ class ChassisController:
         self.motor_controller_2 = motor_controller_2
 
 
+    def set_bl_wheel(self,speed : int = 15000):
+        if (speed >10000):
+            print("FR FORWARD")
+            self.motor_controller_1.wheel_1_forward()
+        elif (speed < -10000):
+            print("FR BACKWARD")
+            self.motor_controller_1.wheel_1_backward()
+        else:
+            self.motor_controller_1.wheel_1_stop()
+        self.motor_controller_1.PWM1.duty_cycle = abs(speed)
 
-    def forward(self,speed = ""):
-        if(speed != ""):
-            self.motor_controller_1.set_speed(int(speed*0xFFFF))
-            self.motor_controller_2.set_speed(int(speed*0xFFFF))
-        self.motor_controller_1.forward()
-        self.motor_controller_2.forward()
+    def set_fr_wheel(self, speed : int = 15000):
+        if (speed >10000):
+            print("BR FORWARD", speed)
+            self.motor_controller_1.wheel_2_forward()
+        elif (speed < -10000):
+            print("BR BACKWARD")
+            self.motor_controller_1.wheel_2_backward()
+        else:
+            self.motor_controller_1.wheel_2_stop()
+        self.motor_controller_1.PWM2.duty_cycle = abs(speed)
 
-    def backward(self,speed):
-        self.motor_controller_1.backward(int(speed*0xFFFF))
-        self.motor_controller_2.backward(int(speed*0xFFFF))
+    def set_fl_wheel(self,speed : int = 15000):
+        if (speed >10000):
+            print("FL FORWARD")
+            self.motor_controller_2.wheel_1_forward()
+        elif (speed < -10000):
+            print("FL BACKWARD")
+            self.motor_controller_2.wheel_1_backward()
+        else:
+            self.motor_controller_2.wheel_1_stop()
+        self.motor_controller_2.PWM1.duty_cycle = abs(speed)
 
-    def left(self,speed):
-        self.motor_controller_1.left(int(speed*0xFFFF))
-        self.motor_controller_2.left(int(speed*0xFFFF))
+    def set_br_wheel(self, speed : int = 15000):
+        if (speed > 10000):
+            print("BL FORWARD")
+            self.motor_controller_2.wheel_2_forward()
+        elif (speed < -10000):
+            print("BL BACKWARD")
+            self.motor_controller_2.wheel_2_backward()
+        else:
+            self.motor_controller_2.wheel_2_stop()
+        self.motor_controller_2.PWM2.duty_cycle = abs(speed)
 
 
-    def right(self,speed):
-        self.motor_controller_1.right(int(speed*0xFFFF))
-        self.motor_controller_2.right(int(speed*0xFFFF))
+
+    # def forward(self,speed = ""):
+    #     if(speed != ""):
+    #         self.motor_controller_1.set_speed(int(speed))
+    #         self.motor_controller_2.set_speed(int(speed))
+    #     self.motor_controller_1.forward()
+    #     self.motor_controller_2.forward()
+
+    # def backward(self,speed):
+    #     self.motor_controller_1.backward(int(speed))
+    #     self.motor_controller_2.backward(int(speed))
+
+    # def left(self,speed):
+    #     self.motor_controller_1.left(int(speed))
+    #     self.motor_controller_2.right(int(speed))
 
 
+    # def right(self,speed):
+    #     self.motor_controller_1.left(int(speed))
+    #     self.motor_controller_2.right(int(speed))
 
-    
