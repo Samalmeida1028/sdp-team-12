0a1a5286de7639b4313f7d78b25b017e8c5a6987
SDP Team 12 <sdpteam12.2023@yahoo.com>
2023-11-16 00:10:30 -0500

    SLAM working!!!

--- basic_mobile_robot/launch/robot.launch.py
+++ basic_mobile_robot/launch/robot.launch.py
@@ -78,7 +78,6 @@ def generate_launch_description():
     ld.add_action(start_robot_localization_cmd)
     ld.add_action(start_robot_state_publisher_cmd)
     ld.add_action(start_joint_state_publisher_cmd)
-    #ld.add_action(static_transform_publisher_cmd)
     ld.add_action(start_rviz_cmd)
 
     return ld
