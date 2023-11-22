8e6640c6cb4c365427c127cc9bffad2457cb2fb4
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2023-11-22 15:28:44 -0500

    refactoring

--- build/basic_mobile_robot/CMakeFiles/basic_mobile_robot_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/basic_mobile_robot_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/basic_mobile_robot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
