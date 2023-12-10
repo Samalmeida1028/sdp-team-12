049d62f37f2022f725d3a28cf0786b7287c1e771
SDP Team 12 <sdpteam12.2023@yahoo.com>
2023-12-09 19:00:39 -0500

    test out behaviors

--- install/basic_mobile_robot/share/basic_mobile_robot/launch/navigation_launch.py
+++ install/basic_mobile_robot/share/basic_mobile_robot/launch/navigation_launch.py
@@ -45,7 +45,6 @@ def generate_launch_description():
         'smoother_server',
         'planner_server',
         'behavior_server',
-        'recoveries_server',
         'bt_navigator',
         'waypoint_follower',
         'velocity_smoother',
@@ -208,13 +207,6 @@ def generate_launch_description():
             #     name='amcl',
             #     output='screen'
             # ),
-            Node(
-                package='nav2_recoveries',
-                executable='recoveries_server',
-                name='recoveries_server',
-                parameters=[configured_params],
-                output='screen'
-            ),
             Node(
                 package='nav2_lifecycle_manager',
                 executable='lifecycle_manager',
