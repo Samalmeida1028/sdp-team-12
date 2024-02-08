cc5d601a2c6a501d62aa24ef9f37f6d33b20698a
SDP Team 12 <sdpteam12.2023@yahoo.com>
2024-02-08 14:48:41 -0500

    integration testing checkpoint 1

--- build/navigator/build/lib/navigator/nav2pose.py
+++ build/navigator/build/lib/navigator/nav2pose.py
@@ -6,6 +6,7 @@
 
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Float32MultiArray
+from nav_msgs.msg import Odometry
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 import rclpy
 from rclpy.node import Node
@@ -18,19 +19,14 @@ class Nav2Pose(Node):
         super().__init__('nav2pose')
         print("Initializing Nav Simple Commander...")
 
-        self.navigator = BasicNavigator()
+        # self.navigator = BasicNavigator()
 
-        print("Setting poses...")
-        self.initial_pose = PoseStamped()
+        self.current_pose = PoseStamped()
         self.goal = PoseStamped()
-
-        self.init_navigator(0.0, 0.0, 1.0)
-        self.prev_goal = self.initial_pose
-        self.publisher = self.create_publisher(PoseStamped, "/goal_pose", 10)
 
         # Wait for navigation to fully activate, since autostarting nav2
         #self.navigator.lifecycleStartup()
-        self.navigator.waitUntilNav2Active(localizer='bt_navigator')
+        # self.navigator.waitUntilNav2Active(localizer='bt_navigator')
 
         self.i = 0
         self.startnav = False
@@ -39,53 +35,48 @@ class Nav2Pose(Node):
 
         print("Creating subscribers and callbacks...")
         self.coordsub = self.create_subscription(Float32MultiArray, '/target_position', self.setgoal, 10)
+        self.odomsub = self.create_subscription(Odometry, '/odometry/filtered', self.set_current_pose, 10)
+        self.goalupdaterpub = self.create_publisher(PoseStamped, "/goal_update", 10)
         
         time_period = 0.5
         self.timer = self.create_timer(time_period, self.nav2pose_callback)
 
         print("Ready!")
 
-    def init_navigator(self, x, y, w):
-        # Set our demo's initial pose
-        self.initial_pose.header.frame_id = 'map'
-        self.initial_pose.header.stamp = self.navigator.get_clock().now().to_msg()
-        self.initial_pose.pose.position.x = x
-        self.initial_pose.pose.position.y = y
-        self.initial_pose.pose.orientation.w = w
+    def set_current_pose(self, odommsg):
+        self.current_pose.header.frame_id = 'map'
+        self.current_pose.header.stamp = self.navigator.get_clock().now().to_msg()
 
-        self.navigator.setInitialPose(self.initial_pose)
+        self.current_pose.pose.position = odommsg.pose.pose.position
+        self.current_pose.pose.orientation = odommsg.pose.pose.orientation
 
-        # If desired, you can change or load the map as well
-        # self.navigator.changeMap('/path/to/map.yaml')
-
-        # You may use the navigator to clear or obtain costmaps
-        # navigator.clearAllCostmaps()  # also have clearLocalCostmap() and clearGlobalCostmap()
-        # global_costmap = navigator.getGlobalCostmap()
-        # local_costmap = navigator.getLocalCostmap()
+        print("Updated current pose!")
 
     def setgoal(self, goalmsg):
-        # goalmsg = [xpos, ypos, dist, xangle, yangle]
+        # goalmsg = [dist, xangle, yangle]
         self.goal.header.frame_id = 'map'
         self.goal.header.stamp = self.navigator.get_clock().now().to_msg()
 
         d = goalmsg.data[0]*math.cos(math.radians(goalmsg.data[2])) # true dist = seen dist * sin(yangle)
         print(goalmsg)
         
-        self.goal.pose.position.x = self.prev_goal.pose.position.x + (d*math.cos(math.radians(goalmsg.data[1])))/1000 # xf = xi + dsin(phi)
-        self.goal.pose.position.y = self.prev_goal.pose.position.y + (d*math.sin(math.radians(goalmsg.data[1])))/1000 # yf = yi + dcos(phi)
+        self.goal.pose.position.x = self.current_pose.pose.position.x + (d*math.cos(math.radians(goalmsg.data[1])))/1000 # xf = xi + dsin(phi)
+        self.goal.pose.position.y = self.current_pose.pose.position.y + (d*math.sin(math.radians(goalmsg.data[1])))/1000 # yf = yi + dcos(phi)
         print(self.goal.pose.position)
 
         rot = Rotation.from_euler('xyz', [0, 0, goalmsg.data[1]], degrees=True)
         rot_quat = rot.as_quat() # convert angle to quaternion format
 
-        self.goal.pose.orientation.x = self.prev_goal.pose.orientation.x + rot_quat[0] # set the orientation to be looking at the marker at the end of navigation
-        self.goal.pose.orientation.y = self.prev_goal.pose.orientation.x + rot_quat[1]
-        self.goal.pose.orientation.z = self.prev_goal.pose.orientation.x + rot_quat[2]
-        self.goal.pose.orientation.w = self.prev_goal.pose.orientation.x + rot_quat[3]
+        self.goal.pose.orientation.x = self.current_pose.pose.orientation.x + rot_quat[0] # set the orientation to be looking at the marker at the end of navigation
+        self.goal.pose.orientation.y = self.current_pose.pose.orientation.x + rot_quat[1]
+        self.goal.pose.orientation.z = self.current_pose.pose.orientation.x + rot_quat[2]
+        self.goal.pose.orientation.w = self.current_pose.pose.orientation.x + rot_quat[3]
 
-        if self.goal != self.prev_goal:
+        if self.goal != self.current_pose:
             self.startnav = True
             print("Updated goal pose!")
+        else:
+            self.startnav = False
 
     def nav2pose_callback(self):
         if self.startnav:
@@ -94,33 +85,32 @@ class Nav2Pose(Node):
             # smooth_path = self.navigator.smoothPath(path)
             # self.navigator.followPath(smooth_path)
 
-            if self.goal != self.prev_goal:
-                # self.publisher.publish(self.goal)
-                # self.prev_goal = self.goal
-                self.navigator.goToPose(self.goal, behavior_tree='/home/adam/Desktop/sdp-team-12/basic_mobile_robot/behavior_trees/navigate_to_pose_w_replanning_and_recovery.xml')
-                self.prev_goal = self.goal
+            if self.current_pose != self.goal:
+                self.goalupdaterpub.publish(self.goal)
+                # self.navigator.goToPose(self.goal, behavior_tree='/home/adam/Desktop/sdp-team-12/basic_mobile_robot/behavior_trees/navigate_to_pose_w_replanning_and_recovery.xml')
+                # self.current_pose = self.goal
 
-            if not self.navigator.isTaskComplete():
-                # Do something with the feedback
-                self.i = self.i + 1
-                feedback = self.navigator.getFeedback()
+            # if not self.navigator.isTaskComplete():
+            #     # Do something with the feedback
+            #     self.i = self.i + 1
+            #     feedback = self.navigator.getFeedback()
 
-                if feedback and self.i % 5 == 0:
-                    print('Estimated time of arrival: ' + '{0:.0f}'.format(
-                        Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
-                        + ' seconds.')
+            #     if feedback and self.i % 5 == 0:
+            #         print('Estimated time of arrival: ' + '{0:.0f}'.format(
+            #             Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
+            #             + ' seconds.')
 
-                    # Some navigation timeout to cancellation
-                    if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
-                        self.navigator.cancelTask()
+            #         # Some navigation timeout to cancellation
+            #         if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
+            #             self.navigator.cancelTask()
 
-            result = self.navigator.getResult()
-            if result == TaskResult.SUCCEEDED:
-                print('Goal succeeded!')
-            elif result == TaskResult.CANCELED:
-                print('Goal was canceled!')
-            elif result == TaskResult.FAILED:
-                print('Goal failed!')
+            # result = self.navigator.getResult()
+            # if result == TaskResult.SUCCEEDED:
+            #     print('Goal succeeded!')
+            # elif result == TaskResult.CANCELED:
+            #     print('Goal was canceled!')
+            # elif result == TaskResult.FAILED:
+            #     print('Goal failed!')
 
 def main(args=None):
     rclpy.init(args=args)
