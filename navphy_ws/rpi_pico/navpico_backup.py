bffcf145a8c593fd6c9b1680a63a9f77d5beab72
SDP Team 12 <sdpteam12.2023@yahoo.com>
2024-02-26 15:09:05 -0500

    backup pico code

--- navphy_ws/rpi_pico/navpico_backup.py
+++ navphy_ws/rpi_pico/navpico_backup.py
@@ -17,7 +17,7 @@ import busio
 ################################################################
 TOP_SPEED = 65536
 WHEEL_SEP = 0.3683
-WHEEL_RADIUS = 0.0508
+WHEEL_RADIUS = 0.0476
 
 serial = usb_cdc.data
 serial.write_timeout = 1
@@ -47,13 +47,14 @@ D3 = board.GP10
 D4 = board.GP11
 D5 = board.GP17
 D6 = board.GP16
+
 # D7 = board.GP18
 # D8 = board.GP22
 
 driver1 = DFR0601(
-    PWM1, INB1, INA1, PWM3, INB3, INA3, 5000
+    PWM1, INB1, INA1, PWM2, INB2, INA2, 5000
 )  # wiring is different, so calling opposite pins 11/14/23
-driver2 = DFR0601(PWM2, INB2, INA2, PWM4, INB4, INA4, 5000)
+driver2 = DFR0601(PWM3, INB3, INA3, PWM4, INB4, INA4, 5000)
 
 chassis = ChassisController(driver1, driver2)
 
@@ -69,66 +70,76 @@ last_position = None
 
 data_in = []
 while True:
-    # print(chassis.motor_controller_1.INA1.value, chassis.motor_controller_1.INB1.value)
+    #print(chassis.motor_controller_1.INA1.value, chassis.motor_controller_1.INB1.value)
     if serial.in_waiting > 0:
         data_in = serial.readline()
-        # print(data_in.decode())
-    if data_in:
-        teleop = json.loads(data_in.decode("utf-8"))
-        print(teleop)
+        if data_in:
+            print(data_in.decode())
+            if json.loads(data_in.decode()) == "Type":
+                # print(data_in)
+                # print("nav")
+                serial.write(bytearray(json.dumps("nav")+"\n"))
+                serial.flush()
+            else:
+                teleop = json.loads(data_in.decode("utf-8"))
+                #print(teleop)
 
-        # mag = math.sqrt(teleop[1]**2 + teleop[0]**2)
-        # phase = math.atan2(teleop[0],teleop[1])
-        # print(teleop)
-        if teleop:
-            #print(teleop)
-            K = 30000
-            lin_x = teleop[0]
-            ang_z = teleop[1]
+                # mag = math.sqrt(teleop[1]**2 + teleop[0]**2)
+                # phase = math.atan2(teleop[0],teleop[1])
+                # print(teleop)
+                if teleop:
+                    #print(teleop)
+                    K_p = 35000
+                    K_z = 5
+                    K_l = 1.25
 
-            v1 = int(-K * (lin_x + (ang_z * (WHEEL_SEP / 2))))
-            v2 = int(-K * (lin_x - (ang_z * (WHEEL_SEP / 2))))
-            v3 = v1
-            v4 = v2
+                    lin_x = teleop[0]
+                    ang_z = teleop[1]
 
-            motors = [v1, v2, v3, v4]
-            print(teleop, motors)
+                    v1 = (int(-K_p * ((lin_x*K_l) + ((ang_z*K_z) * (WHEEL_SEP / 2)))))
+                    v2 = int(-K_p * ((lin_x*K_l) - ((ang_z*K_z) * (WHEEL_SEP / 2))))
+                    v3 = v1
+                    v4 = v2
 
-            chassis.set_fl_wheel(v1)
-            chassis.set_fr_wheel(v2)
-            chassis.set_bl_wheel(v3)
-            chassis.set_br_wheel(v4)
-        else:
-            v1 = 0
-            v2 = 0
-            v3 = 0
-            v4 = 0
-            motors = [v1, v2, v3, v4]
-            chassis.set_fl_wheel(v1)
-            chassis.set_fr_wheel(v2)
-            chassis.set_bl_wheel(v3)
-            chassis.set_br_wheel(v4)
+                    motors = [v1, v2, v3, v4]
+                    # print(teleop, motors)
 
-    encoder_data.update({"BL": enc1.get_data(time.monotonic_ns())})
-    encoder_data.update({"FL": enc2.get_data(time.monotonic_ns())})
-    encoder_data.update({"FR": enc3.get_data(time.monotonic_ns())})
+                    chassis.set_fl_wheel(v1)
+                    chassis.set_fr_wheel(v2)
+                    chassis.set_bl_wheel(v3)
+                    chassis.set_br_wheel(v4)
+                else:
+                    v1 = 0
+                    v2 = 0
+                    v3 = 0
+                    v4 = 0
+                    motors = [v1, v2, v3, v4]
+                    chassis.set_fl_wheel(v1)
+                    chassis.set_fr_wheel(v2)
+                    chassis.set_bl_wheel(v3)
+                    chassis.set_br_wheel(v4)
 
-    imu_data = sox.gyro
-    imu_data2 = sox.acceleration
-    sensor_data = {
-        "Encoder": encoder_data,
-        "IMU": {
-            "Gyro": {"r": imu_data[0], "p": imu_data[1], "y": -imu_data[2]},
-            "Accel": {"x": imu_data2[0], "y": imu_data2[1], "z": -imu_data2[2]},
-        },
-    }
-    remaining = bytes(json.dumps(sensor_data) + "\n", "utf-8")
-    # print(sensor_data["Encoder"])
-    # print(sensor_data["Encoder"]["BL"]["Vel"], 
-    #       sensor_data["Encoder"]["FL"]["Vel"], 
-    #       sensor_data["Encoder"]["FR"]["Vel"])
 
-    n = serial.write(remaining)
-    #print(n)
-    while serial.out_waiting:
-        pass
+                encoder_data.update({"BL": enc1.get_data(time.monotonic_ns())})
+                encoder_data.update({"FL": enc2.get_data(time.monotonic_ns())})
+                encoder_data.update({"FR": enc3.get_data(time.monotonic_ns())})
+
+                imu_data = sox.gyro
+                imu_data2 = sox.acceleration
+                sensor_data = {
+                    "Encoder": encoder_data,
+                    "IMU": {
+                        "Gyro": {"r": imu_data[0], "p": imu_data[1], "y": -imu_data[2]},
+                        "Accel": {"x": imu_data2[0], "y": imu_data2[1], "z": -imu_data2[2]},
+                    },
+                }
+                remaining = bytes(json.dumps(sensor_data) + "\n", "utf-8")
+                # print(sensor_data["Encoder"])
+                print(sensor_data["Encoder"]["BL"]["Pos"], 
+                    sensor_data["Encoder"]["FL"]["Pos"], 
+                    sensor_data["Encoder"]["FR"]["Pos"])
+
+                n = serial.write(remaining)
+                #print(n)
+                while serial.out_waiting:
+                    pass
