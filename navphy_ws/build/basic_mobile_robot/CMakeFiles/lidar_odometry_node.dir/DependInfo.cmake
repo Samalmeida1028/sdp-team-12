09b6779bfb77d974a82a51fcff96725ef05a3686
SDP Team 12 <sdpteam12.2023@yahoo.com>
2024-03-27 14:43:33 -0400

    testing on laptop 3/27/24

--- navphy_ws/build/basic_mobile_robot/CMakeFiles/lidar_odometry_node.dir/DependInfo.cmake
+++ navphy_ws/build/basic_mobile_robot/CMakeFiles/lidar_odometry_node.dir/DependInfo.cmake
@@ -8,8 +8,8 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/sdpteam12/Desktop/sdp-team-12/navphy_ws/basic_mobile_robot/src/lidar_odometry.cpp" "CMakeFiles/lidar_odometry_node.dir/src/lidar_odometry.cpp.o" "gcc" "CMakeFiles/lidar_odometry_node.dir/src/lidar_odometry.cpp.o.d"
-  "/home/sdpteam12/Desktop/sdp-team-12/navphy_ws/basic_mobile_robot/src/lidar_odometry_node.cpp" "CMakeFiles/lidar_odometry_node.dir/src/lidar_odometry_node.cpp.o" "gcc" "CMakeFiles/lidar_odometry_node.dir/src/lidar_odometry_node.cpp.o.d"
+  "/home/adam/Desktop/sdp-team-12/navphy_ws/basic_mobile_robot/src/lidar_odometry.cpp" "CMakeFiles/lidar_odometry_node.dir/src/lidar_odometry.cpp.o" "gcc" "CMakeFiles/lidar_odometry_node.dir/src/lidar_odometry.cpp.o.d"
+  "/home/adam/Desktop/sdp-team-12/navphy_ws/basic_mobile_robot/src/lidar_odometry_node.cpp" "CMakeFiles/lidar_odometry_node.dir/src/lidar_odometry_node.cpp.o" "gcc" "CMakeFiles/lidar_odometry_node.dir/src/lidar_odometry_node.cpp.o.d"
   )
 
 # Targets to which this target links.
