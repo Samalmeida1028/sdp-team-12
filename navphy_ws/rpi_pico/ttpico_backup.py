26a6f578cbd5897471acb8ccf708206433978c1e
sdp-team-12 <sdpteam12@yahoo.com>
2024-04-04 11:24:07 -0400

    backup target tracking pico code

--- navphy_ws/rpi_pico/ttpico_backup.py
+++ navphy_ws/rpi_pico/ttpico_backup.py
@@ -11,15 +11,15 @@ import digitalio
 ################################################################
 
 serial = usb_cdc.data
-pwm1 = pwmio.PWMOut(board.GP1, duty_cycle=2 ** 15, frequency=50)
-pwm2 = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 12, frequency=50)
-led = digitalio.DigitalInOut(board.LED)
+pwm1 = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 8, frequency=330)
+pwm2 = pwmio.PWMOut(board.GP1, duty_cycle=2 ** 8, frequency=330)
+led = digitalio.DigitalInOut(board.GP21)
 led.direction = digitalio.Direction.OUTPUT
 my_servoy = servo.Servo(pwm2)
 my_servox = servo.Servo(pwm1)
 center = 90
 my_servox.angle = center
-my_servoy.angle = center
+my_servoy.angle = 180-center
 
 tempx = center
 tempy = center
@@ -28,15 +28,16 @@ k_x = 2
 k_y = 1.5
 integral_x = 0
 integral_y = 0
-d_x = 8
-d_y = 6
+d_x = 12
+d_y = 10
 vel = (0, 0)
 
 # flag = 1
-prev_time = time.time()
+# prev_time = time.time()
 offset = 0
 
 dt = time.monotonic_ns()*1e-14
+current_time = time.monotonic_ns()
 while True:
     dt = (time.monotonic_ns()*1e-14)-dt
     # print("hi")
@@ -56,20 +57,24 @@ while True:
                 # print("Asking type")
                 serial.write(bytearray(json.dumps("tracking") + "\n"))
             else:
-                t = time.time() - prev_time
-                if t > 0.5:
-                    led.value ^= 1
-                    prev_time = time.time()
 
                 translation = json.loads(data_in.decode('utf-8'))
-                print(translation)
-
                 if translation == "center":
                     my_servox.angle = center
-                    my_servoy.angle = center + 10
+                    my_servoy.angle = center - 5
+                    led.value = 0
 
                 else:
                     vel = (float(translation[0])*(1080/1920),float(translation[1])*(1080/1920))
+                    led_state = int(translation[2])
+                    print(translation)
+                    if(led_state == 1):
+                        led.value = True
+                    elif led_state == 2:
+                        if(time.monotonic_ns() - current_time > .5):
+                            current_time = time.monotonic_ns()
+                            led.value = not led.value
+
                 
                 # print(desired_angle)
                 # error_x = (desired_angle[0]-my_servox.angle)
@@ -79,13 +84,13 @@ while True:
                 # data_inx = min(180,max(-180,data_inx))
                 # data_iny = min(180,max(-180,data_iny))
                 # print(my_servox.angle,my_servoy.angle, "data:",data_inx,data_iny)
-                    data_inx += vel[0]*dt*k_x + d_x*dt*9
-                    data_iny -= vel[1]*dt*k_y + d_y*dt*6
+                    data_inx -= vel[0]*dt*k_x + d_x*dt*10
+                    data_iny += vel[1]*dt*k_y + d_y*dt*8
 
                     my_servox.angle = min(180,max(0,int(data_inx)))
                     my_servoy.angle = min(120,max(0,int(data_iny)))  
                 
-                    myangle = [90-my_servox.angle,my_servoy.angle-90]
+                    myangle = [my_servox.angle-90,my_servoy.angle-90]
                     # print(dt,myangle)
 
                     serial.write(bytearray(json.dumps(myangle) + "\n", "utf-8"))
